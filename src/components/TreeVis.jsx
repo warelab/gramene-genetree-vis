0d680025bf1dfd2df568815ebdaddd340d41fb8d
Andrew Olson <olson@cshl.edu>
2016-09-14 15:54:02 -0400

    restore paralogs code

--- src/components/TreeVis.jsx
+++ src/components/TreeVis.jsx
@@ -219,6 +219,36 @@ var TreeVis = React.createClass({
     this.setState({hoveredNode: node});
   },
 
+  handleNodeShowParalogs: function (node) {
+    function expanded(n) {
+      return !n.parent || n.parent.displayInfo.expanded;
+    }
+    if (!!node.displayInfo.paralogs) {
+      var genetree = this.genetree;
+      var additionalVisibleNodes = _.clone(this.state.additionalVisibleNodes);
+      node.displayInfo.paralogs.forEach(function(paralog) {
+        var parentNode = paralog.parent;
+        while (!parentNode.displayInfo.expanded) {
+          parentNode.displayInfo.expanded = true;
+          parentNode.displayInfo.expandedBecause = 'selected'; // this happens again in layoutTree()
+          additionalVisibleNodes[parentNode.model.node_id] = parentNode;
+          parentNode = parentNode.parent
+        }
+      });
+      var allVisibleNodes = layoutTree(
+        this.genetree,
+        this.state.geneOfInterest,
+        this.treeWidth,
+        additionalVisibleNodes
+      );
+      this.reinitHeight();
+      this.setState({
+        additionalVisibleNodes: additionalVisibleNodes,
+        visibleNodes: allVisibleNodes
+      });
+    }
+  },
+  
   renderBackground: function () {
     if (this.displayAlignments) {
       var bgStyle = {fill: '#f7f7f7', stroke: false};
